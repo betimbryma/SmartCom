6cf79ec90290337f21811e646c33d8707def2df2
Philipp Zeppezauer <philipp.zeppezauer@gmail.com>
2014-06-16 10:20:45 +0200

    message broker documentation

--- core/src/main/java/at/ac/tuwien/dsg/smartcom/broker/MessageBroker.java
+++ core/src/main/java/at/ac/tuwien/dsg/smartcom/broker/MessageBroker.java
@@ -20,6 +20,13 @@ public interface MessageBroker extends FeedbackPublisher {
      */
     public Message receiveFeedback();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new feedback messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for feedback messages
+     */
     public void registerFeedbackListener(MessageListener listener);
 
     /**
@@ -31,6 +38,15 @@ public interface MessageBroker extends FeedbackPublisher {
      */
     public Message receiveRequests(String id);
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new request messages for a given receiver. Note that a message listener
+     * will consume the message and that there can only be one listener at a
+     * time for each id.
+     *
+     * @param id the id of the request receiver
+     * @param listener for request messages
+     */
     public void registerRequestListener(String id, MessageListener listener);
 
     /**
@@ -41,39 +57,153 @@ public interface MessageBroker extends FeedbackPublisher {
      */
     public void publishRequest(String id, Message message);
 
+    /**
+     * Receives task messages for a given id. Might block until there
+     * is a message available.
+     *
+     * @param id that the task message is assigned to
+     * @return the message
+     */
     public Message receiveTasks(String id);
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new task messages for a given receiver. Note that a message listener
+     * will consume the message and that there can only be one listener at a
+     * time for each id.
+     *
+     * @param id the id of the task receiver
+     * @param listener for task messages
+     */
     public void registerTaskListener(String id, MessageListener listener);
 
+    /**
+     * Publish a task message for a given receiver destination id (note that
+     * this is not the same as the receiver id! One receiver id can be resolved
+     * to many receiver destination ids).
+     *
+     * @param id of the receiver destination
+     * @param message that should be published
+     */
     public void publishTask(String id, Message message);
 
+    /**
+     * Publish a control message.
+     *
+     * @param message control message
+     */
     public void publishControl(Message message);
 
+    /**
+     * Receives control messages. Might block until there
+     * is a message available.
+     *
+     * @return the message
+     */
     public Message receiveControl();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new control messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for control messages
+     */
     public void registerControlListener(MessageListener listener);
 
+    /**
+     * Publish authentication request message.
+     *
+     * @param message that should be published
+     */
     public void publishAuthRequest(Message message);
 
+    /**
+     * Receives authentication messages. Might block until there
+     * is a message available.
+     *
+     * @return the message
+     */
     public Message receiveAuthRequest();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new authentication messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for authentication messages
+     */
     public void registerAuthListener(MessageListener listener);
 
+    /**
+     * publish message info request message.
+     *
+     * @param message that should be published
+     */
     public void publishMessageInfoRequest(Message message);
 
+    /**
+     * Receives message info request messages. Might block until there
+     * is a message available.
+     *
+     * @return the message
+     */
     public Message receiveMessageInfoRequest();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new message info request messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for message info request messages
+     */
     public void registerMessageInfoListener(MessageListener listener);
 
+    /**
+     * publishes a metrics request message.
+     *
+     * @param message that should be published
+     */
     public void publishMetricsRequest(Message message);
 
+    /**
+     * Receives metrics request messages. Might block until there
+     * is a message available.
+     *
+     * @return the message
+     */
     public Message receiveMetricsRequest();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new metrics request messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for metrics request messages
+     */
     public void registerMetricsListener(MessageListener listener);
 
+    /**
+     * publishes a log message
+     *
+     * @param message that should be published
+     */
     public void publishLog(Message message);
 
+    /**
+     * Receives log messages. Might block until there
+     * is a message available.
+     *
+     * @return the message
+     */
     public Message receiveLog();
 
+    /**
+     * Register a message listener that will be notified on the arrival of
+     * new log messages. Note that a message listener will consume the message
+     * and that there can only be one listener at a time.
+     *
+     * @param listener for log messages
+     */
     public void registerLogListener(MessageListener listener);
 }

bc8c6988ed39da3cdab2f7a3596d6e1eed03ed84
Philipp Zeppezauer <philipp.zeppezauer@gmail.com>
2014-08-22 16:02:05 +0200

    added license

--- adapter/src/main/java/at/ac/tuwien/dsg/smartcom/manager/am/AdapterManagerImpl.java
+++ adapter/src/main/java/at/ac/tuwien/dsg/smartcom/manager/am/AdapterManagerImpl.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2014 Technische Universitat Wien (TUW), Distributed Systems Group E184 (http://dsg.tuwien.ac.at)
+ *
+ * This work was partially supported by the EU FP7 FET SmartSociety (http://www.smart-society-project.eu/).
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package at.ac.tuwien.dsg.smartcom.manager.am;
 
 import at.ac.tuwien.dsg.smartcom.adapter.InputAdapter;
@@ -234,7 +251,7 @@ public class AdapterManagerImpl implements AdapterManager {
                                     case TO_ALL_CHANNELS:
                                         continue addressLoop;
                                     case AT_LEAST_ONE:
-                                        break addressLoop;
+                                        continue addressLoop;
                                     case PREFERRED:
                                         break addressLoop;
                                 }
@@ -264,7 +281,7 @@ public class AdapterManagerImpl implements AdapterManager {
                         case TO_ALL_CHANNELS:
                             continue addressLoop;
                         case AT_LEAST_ONE:
-                            break addressLoop;
+                            continue addressLoop;
                         case PREFERRED:
                             break addressLoop;
                     }
--- api/src/main/java/at/ac/tuwien/dsg/smartcom/callback/PeerInfoCallback.java
+++ api/src/main/java/at/ac/tuwien/dsg/smartcom/callback/PeerInfoCallback.java
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2014 Technische Universitat Wien (TUW), Distributed Systems Group E184 (http://dsg.tuwien.ac.at)
+ *
+ * This work was partially supported by the EU FP7 FET SmartSociety (http://www.smart-society-project.eu/).
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package at.ac.tuwien.dsg.smartcom.callback;
 
 import at.ac.tuwien.dsg.smartcom.callback.exception.NoSuchPeerException;
@@ -5,6 +22,9 @@ import at.ac.tuwien.dsg.smartcom.model.Identifier;
 import at.ac.tuwien.dsg.smartcom.model.PeerInfo;
 
 /**
+ * This callback is used to resolve peer information upon a peer. This information does not change very
+ * often but is queried quite frequently, therefore retrieved data should be cached as long as the callback does
+ * not provide the required performance throughput.
  *
  * @author Philipp Zeppezauer (philipp.zeppezauer@gmail.com)
  * @version 1.0
@@ -14,7 +34,7 @@ public interface PeerInfoCallback {
      * Resolves the information of a given peer (e.g., provides the address and the method/adapter that should be used).
      *
      * @param id id of the requested peer
-     * @return Returns information upon a peer
+     * @return Returns information about a peer
      * @throws NoSuchPeerException if there exists no such peer.
      */
     public PeerInfo getPeerInfo(Identifier id) throws NoSuchPeerException;
